5c68a6a4108cb5644d1bf10839ecb738a29c8aa5
Dahyun Noh <dk3146@naver.com>
2024-05-10 16:44:30 +0900

    [style] set pep8 convention

--- Morai_Project/scripts/gpsimu_parser.py
+++ Morai_Project/scripts/gpsimu_parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
- 
+
 import rospy
 import tf
 import os
@@ -15,24 +15,26 @@ from math import pi
 class GPSIMUParser:
     def __init__(self):
         rospy.init_node('GPS_IMU_parser', anonymous=True)
-        self.gps_sub = rospy.Subscriber("/gps", GPSMessage, self.navsat_callback)
+        self.gps_sub = rospy.Subscriber(
+            "/gps", GPSMessage, self.navsat_callback)
         self.imu_sub = rospy.Subscriber("/imu", Imu, self.imu_callback)
-        self.odom_pub = rospy.Publisher('/odom',Odometry, queue_size=1)
+        self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size=1)
         # 초기화
         self.x, self.y = None, None
-        self.is_imu=False
-        self.is_gps=False
+        self.is_imu = False
+        self.is_gps = False
 
-        self.proj_UTM = Proj(proj='utm',zone=52, ellps='WGS84', preserve_units=False)
+        self.proj_UTM = Proj(proj='utm', zone=52,
+                             ellps='WGS84', preserve_units=False)
 
-        self.odom_msg=Odometry()
-        self.odom_msg.header.frame_id='/odom'
-        self.odom_msg.child_frame_id='/base_link'
+        self.odom_msg = Odometry()
+        self.odom_msg.header.frame_id = '/odom'
+        self.odom_msg.child_frame_id = '/base_link'
 
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             os.system('clear')
-            if self.is_imu==True and self.is_gps == True:
+            if self.is_imu == True and self.is_gps == True:
                 self.convertLL2UTM()
                 self.odom_pub.publish(self.odom_msg)
                 print(f"odom_msg is now being published at '/odom' topic!\n")
@@ -40,10 +42,12 @@ class GPSIMUParser:
                 print(self.odom_msg.pose)
 
             if not self.is_imu:
-                print("[1] can't subscribe '/imu' topic... \n    please check your IMU sensor connection")
+                print(
+                    "[1] can't subscribe '/imu' topic... \n    please check your IMU sensor connection")
             if not self.is_gps:
-                print("[2] can't subscribe '/gps' topic... \n    please check your GPS sensor connection")
-            
+                print(
+                    "[2] can't subscribe '/gps' topic... \n    please check your GPS sensor connection")
+
             self.is_gps = self.is_imu = False
             rate.sleep()
 
@@ -54,11 +58,10 @@ class GPSIMUParser:
         self.e_o = gps_msg.eastOffset
         self.n_o = gps_msg.northOffset
 
-        self.is_gps=True
+        self.is_gps = True
 
-    def convertLL2UTM(self):    
+    def convertLL2UTM(self):
         xy_zone = self.proj_UTM(self.lon, self.lat)
-
 
         if self.lon == 0 and self.lat == 0:
             self.x = 0.0
@@ -85,7 +88,8 @@ class GPSIMUParser:
             self.odom_msg.pose.pose.orientation.z = data.orientation.z
             self.odom_msg.pose.pose.orientation.w = data.orientation.w
 
-        self.is_imu=True
+        self.is_imu = True
+
 
 if __name__ == '__main__':
     try:
--- Morai_Project/scripts/tf_pub.py
+++ Morai_Project/scripts/tf_pub.py
@@ -8,20 +8,20 @@ from nav_msgs.msg import Odometry
 
 # tf 는 물체의 위치와 자세 데이터를 좌표계로 나타내는 예제입니다.
 
-# 노드 실행 순서 
+# 노드 실행 순서
 # 1. Callback 함수 생성
 # 2. 브로드캐스터 생성 및 Ego 상태 tf 브로드캐스팅
+
 
 class Ego_listener():
     def __init__(self):
         rospy.init_node('status_listener', anonymous=True)
-        
+
         rospy.Subscriber("odom", Odometry, self.odom_callback)
         rospy.spin()
 
-
-    #TODO: (1) Callback 함수 생성
-    def odom_callback(self,msg):
+    # TODO: (1) Callback 함수 생성
+    def odom_callback(self, msg):
         self.is_odom = True
 
         self.x = msg.pose.pose.position.x
@@ -32,16 +32,18 @@ class Ego_listener():
         self.orientation_z = msg.pose.pose.orientation.z
         self.orientation_w = msg.pose.pose.orientation.w
 
-        #TODO: (2) 브로드캐스터 생성 및 Ego 상태 tf 브로드캐스팅
+        # TODO: (2) 브로드캐스터 생성 및 Ego 상태 tf 브로드캐스팅
         br = tf.TransformBroadcaster()
         br.sendTransform((self.x, self.y, 1),
-                        (self.orientation_x,self.orientation_y,self.orientation_z,self.orientation_w),
-                        rospy.Time.now(),
-                        "Ego",
-                        "map")
+                         (self.orientation_x, self.orientation_y,
+                          self.orientation_z, self.orientation_w),
+                         rospy.Time.now(),
+                         "Ego",
+                         "map")
+
 
 if __name__ == '__main__':
     try:
-        tl=Ego_listener()
+        tl = Ego_listener()
     except rospy.ROSInternalException:
         pass
